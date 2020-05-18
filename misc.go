5aa1ae4a1d50fc684bb868d9b037cac7bf48189f
Daniel Horsley <dhorsley@gmail.com>
2020-05-18 01:49:13 +0000

    Final 1.0.3 version.

--- misc.go
+++ misc.go
@@ -71,10 +71,10 @@ func help(hargs string) {
 	switch len(hargs) {
 	case 0:
 		helppage := `
-[#1]{@language} [-v] [-h] [-i] [-m] [-c] [-l] [-S] [-s [#i1]path[#i0]]  \
-    [-t] [-O [#i1]tval[#i0]] [-G [#i1]group_filter[#i0]]        \
-    [-o [#i1]output_file[#i0]]                        \
-    [-r] [-F "[#i1]sep[#i0]"] [-e [#i1]program_string[#i0]]     \
+[#1]{lower("{@language}")} [-v] [-h] [-i] [-m] [-c] [-l] [-S] [-s [#i1]path[#i0]] \
+    [-t] [-O [#i1]tval[#i0]] [-G [#i1]group_filter[#i0]]            \
+    [-o [#i1]output_file[#i0]]                            \
+    [-r] [-F "[#i1]sep[#i0]"] [-e [#i1]program_string[#i0]]         \
     [-T [#i1]time-out[#i0]] [[-f] [#i1]input_file[#i0]][#-]
 
     [#4]-v[#-] : Version
