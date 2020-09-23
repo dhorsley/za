865e2a25fd50b2a7f4db287e5e13e1f8084c4d6f
Daniel Horsley <dhorsley@gmail.com>
2020-09-23 22:15:44 +0000

    more fixes for error source line numbering and interpolation. added { =... } form for evaluation in interpolation.

--- parser.go
+++ parser.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	str "strings"
+    str "strings"
 )
 
 // phraseParse():
@@ -18,20 +18,19 @@ import (
 
 func phraseParse(fs string, input string, start int) (badword bool, eof bool) {
 
-	pos := start
-	lstart := start
+    pos := start
+    lstart := start
 
-	var eol bool
+    var eol bool
 
-	var tempToken Token
-	var phrase = Phrase{}
+    var tempToken Token
+    var phrase = Phrase{}
 
-	previousToken := Error
-	curLine := 0
-    newStatement:=true
+    tokenType := Error
+    curLine := 0
 
-    var strPhrase str.Builder
-    strPhrase.Grow(32)
+    // var strPhrase str.Builder
+    // strPhrase.Grow(32)
 
     // simple handler for parens nesting
     var braceNestLevel  int     // round braces
@@ -40,75 +39,70 @@ func phraseParse(fs string, input string, start int) (badword bool, eof bool) {
 
     lmv,_:=fnlookup.lmget(fs)
 
-    sourceStore[lmv]=make([]string,40)
+    // pf("[parser] Placed source in store %d for fs '%s'\n",lmv,fs)
+    sourceStore[lmv]=make([]string,0,40)
+    addToPhrase:=false
 
-	for ; pos < len(input); pos++ {
+    for ; pos < len(input); {
 
-        // pf("nt : (pos:%d) calling nextToken()\n",pos)
-		tempToken, tokPos, eol, eof = nextToken(input, &curLine, pos, previousToken, newStatement)
-		previousToken = tempToken.tokType
-        // pf("%d->"+tokNames[previousToken]+"\n",curLine)
+        tempToken, tokPos, eol, eof = nextToken(input, &curLine, pos, tokenType)
+        // pf("temptoken [type : %s] -> %#v\n",tokNames[tempToken.tokType],tempToken)
 
-        newStatement=false
+        // If we found something then move the cursor along to next word
+        if tokPos != -1 { pos = tokPos }
 
-        if previousToken==LParen {
+        tokenType = tempToken.tokType
+
+        if tokenType==LParen {
             braceNestLevel++
         }
-        if previousToken==RParen {
+        if tokenType==RParen {
             braceNestLevel--
         }
-        if previousToken==LeftSBrace {
+        if tokenType==LeftSBrace {
             sbraceNestLevel++
         }
-        if previousToken==RightSBrace {
+        if tokenType==RightSBrace {
             sbraceNestLevel--
         }
 
         if sbraceNestLevel>0 || braceNestLevel>0 {
-            if eol || previousToken==EOL {
+            if eol || tokenType==EOL {
+                curLine++
                 continue
             }
         }
 
         // debug(15,"nt-t: (tokpos:%d) %v\n",tokPos,tokNames[tempToken.tokType])
 
-        if previousToken==SingleComment {
-            // at this point we have returned the full comment, pos was backtracked to just before the EOL.
-            tempToken.tokType=EOL
+        if tempToken.tokType == Error {
+            pf("Error found on line %d in %s\n", curLine+1, tempToken.tokText)
+            break
         }
 
-        if previousToken==C_Semicolon {
-            tempToken.tokType=EOL
-            tempToken.tokText=""
-            eol=true
+        addToPhrase = true
+
+
+        if tokenType==SingleComment {
+            // at this point we have returned the full comment
+            // throw comment away, but put it in the sourceStore
+            addToPhrase=false
         }
 
-		phrase.Tokens = append(phrase.Tokens, tempToken)
-		phrase.TokenCount++
-        strPhrase.WriteString(tempToken.tokText+" ")
+        if tokenType==C_Semicolon || tokenType==EOL {
+            addToPhrase=false
+        }
 
-		if tokPos != -1 {
-			pos = tokPos
-		}
 
-		if tempToken.tokType == Error {
-			pf("Error found on line %d in %s\n", curLine+1, tempToken.tokText)
-			break
-		}
+        if addToPhrase {
+            phrase.Tokens = append(phrase.Tokens, tempToken)
+            phrase.TokenCount++
+        }
 
-		if eof || eol {
+        if tempToken.tokType == EOL || tempToken.tokType==C_Semicolon {
 
-            // -- strip the eol
-            if eol {
-                phrase.TokenCount--
-                phrase.Tokens=phrase.Tokens[:phrase.TokenCount]
-            }
-
-			// -- cleanup phrase text
-			// phrase.Text = str.TrimRight(strPhrase.String(), " ")
-
-			// -- add original version
-			if pos>0 {
+            // -- add original version
+            if pos>0 {
                 if phrase.TokenCount>0 {
                     switch phrase.Tokens[0].tokType {
                     case C_On,SYM_BOR:
@@ -121,11 +115,16 @@ func phraseParse(fs string, input string, start int) (badword bool, eof bool) {
                         phrase.Original=input[lstart:pos]
                     }
                 }
-                sourceStore[lmv]=append(sourceStore[lmv],input[lstart:pos])
             }
-			lstart = pos + 1
 
-            phrase.SourceLine=curLine-1
+            // -- add to source store
+            sourceStore[lmv]=append(sourceStore[lmv],str.TrimRight(input[lstart:pos]," \t\n"))
+            phrase.SourceLine=curLine
+            lstart = pos
+
+            // pf("phrase source line set to %d on line: %+q\n",phrase.SourceLine,input[lstart:pos])
+
+            if tempToken.tokType==EOL { curLine++ }
 
             // -- discard empty lines
             if phrase.TokenCount!=0 {
@@ -135,19 +134,20 @@ func phraseParse(fs string, input string, start int) (badword bool, eof bool) {
                 if lockSafety { fspacelock.Unlock() }
             }
 
-            newStatement=true
+            // reset phrase
+            phrase = Phrase{}
 
-			// reset phrase
-			phrase = Phrase{}
-            strPhrase.Reset()
-		}
+            // strPhrase.Reset()
 
-		if eof {
-			break
-		}
+        }
 
-	}
+        if eof {
+            break
+        }
 
-	return badword, eof
+    }
+
+
+    return badword, eof
 
 }
