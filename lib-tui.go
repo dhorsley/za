b3bf6d2c5fc8547cc793666ad0b67d252b803e08
Daniel Horsley <daniel@dh-ubu0.costcutter.local>
2024-08-05 16:54:52 +0100

    started tui_text

--- lib-tui.go
+++ lib-tui.go
@@ -4,6 +4,8 @@ package main
 
 import (
     "fmt"
+	"bytes"
+	"unicode"
     str "strings"
 )
 
@@ -17,6 +19,7 @@ type tui struct {
     cursor  string
     title   string
     prompt  string
+    content string
 }
 
 type tui_style struct {
@@ -33,7 +36,6 @@ type tui_style struct {
 /*
    actions to add:
    horizontal menu
-   display pane (with word splitting)
    progress bar
    input box
    cascading input selector
@@ -53,7 +55,94 @@ func priScreen() {
 }
 
 
-func tui_box(row,col,height,width int,title string,s tui_style) {
+/* mitchell hashimoto word wrap code below
+    from: https://github.com/mitchellh/go-wordwrap/blob/master/wordwrap.go
+   mit licensed.
+   not likely to need an update, so just taking the func.
+   may add a left/right/full justify option to it later.
+*/
+const nbsp = 0xA0
+func WrapString(s string, lim uint) string {
+	// Initialize a buffer with a slightly larger size to account for breaks
+	init := make([]byte, 0, len(s))
+	buf := bytes.NewBuffer(init)
+
+	var current uint
+	var wordBuf, spaceBuf bytes.Buffer
+	var wordBufLen, spaceBufLen uint
+
+	for _, char := range s {
+		if char == '\n' {
+			if wordBuf.Len() == 0 {
+				if current+spaceBufLen > lim {
+					current = 0
+				} else {
+					current += spaceBufLen
+					spaceBuf.WriteTo(buf)
+				}
+				spaceBuf.Reset()
+				spaceBufLen = 0
+			} else {
+				current += spaceBufLen + wordBufLen
+				spaceBuf.WriteTo(buf)
+				spaceBuf.Reset()
+				spaceBufLen = 0
+				wordBuf.WriteTo(buf)
+				wordBuf.Reset()
+				wordBufLen = 0
+			}
+			buf.WriteRune(char)
+			current = 0
+		} else if unicode.IsSpace(char) && char != nbsp {
+			if spaceBuf.Len() == 0 || wordBuf.Len() > 0 {
+				current += spaceBufLen + wordBufLen
+				spaceBuf.WriteTo(buf)
+				spaceBuf.Reset()
+				spaceBufLen = 0
+				wordBuf.WriteTo(buf)
+				wordBuf.Reset()
+				wordBufLen = 0
+			}
+
+			spaceBuf.WriteRune(char)
+			spaceBufLen++
+		} else {
+			wordBuf.WriteRune(char)
+			wordBufLen++
+
+			if current+wordBufLen+spaceBufLen > lim && wordBufLen < lim {
+				buf.WriteRune('\n')
+				current = 0
+				spaceBuf.Reset()
+				spaceBufLen = 0
+			}
+		}
+	}
+
+	if wordBuf.Len() == 0 {
+		if current+spaceBufLen <= lim {
+			spaceBuf.WriteTo(buf)
+		}
+	} else {
+		spaceBuf.WriteTo(buf)
+		wordBuf.WriteTo(buf)
+	}
+
+	return buf.String()
+}
+/* end of theft */
+
+func tui_text(t tui,s tui_style) {
+    // at row,col, width of t.width-2, print wordWrap'd t.content using inset() to move line starts to col+2
+}
+
+
+// func tui_box(row,col,height,width int,title string,s tui_style) {
+func tui_box(t tui,s tui_style) {
+
+    row:=t.row; col:=t.col
+    height:=t.height; width:=t.width
+    title:=t.title
 
     // pf("\n%d,%d,%d,%d,%s,%#v\n",row,col,height,width,title,s)
     tl:=s.border["tl"]
@@ -261,6 +350,7 @@ func buildTuiLib() {
         switch str.ToLower(t.action) {
         case "box"  : stdlib["tui_box"](ns,evalfs,ident,t,s)
         case "menu" : stdlib["tui_menu"](ns,evalfs,ident,t,s)
+        case "text" : stdlib["tui_text"](ns,evalfs,ident,t,s)
         }
         return "",err
     }
@@ -273,9 +363,23 @@ func buildTuiLib() {
         t:=args[0].(tui)
         s:=default_tui_style
         if len(args)==2 { s=args[1].(tui_style) }
-        tui_box(t.row,t.col,t.height,t.width,t.title,s) 
+        // tui_box(t.row,t.col,t.height,t.width,t.title,s) 
+        tui_box(t,s) 
         return nil,err
     }
+
+    slhelp["tui_text"] = LibHelp{in: "tui_struct[,tui_style]", out: "", action: "output text"}
+    stdlib["tui_text"] = func(ns string,evalfs uint32,ident *[]Variable,args ...any) (ret any, err error) {
+        if ok,err:=expect_args("tui_text",args,2,
+            "1","main.tui",
+            "2","main.tui","main.tui_style"); !ok { return nil,err }
+        t:=args[0].(tui)
+        s:=default_tui_style
+        if len(args)==2 { s=args[1].(tui_style) }
+        tui_text(t,s)
+        return nil,err
+    }
+
 
     slhelp["tui_menu"] = LibHelp{in: "tui_struct[,tui_style]", out: "", action: "present menu"}
     stdlib["tui_menu"] = func(ns string,evalfs uint32,ident *[]Variable,args ...any) (ret any, err error) {
