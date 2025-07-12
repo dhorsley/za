659975ca165ac512e7ea4138031b639e2d998329
Daniel Horsley <dhorsley@gmail.com>
2025-07-12 01:14:55 +0100

    added USES clause to TRY. more lib-network.go testing done.

--- console_unix.go
+++ console_unix.go
@@ -1,22 +1,22 @@
+//go:build !windows
 // +build !windows
 
 package main
 
 import (
     "bytes"
-    term "github.com/pkg/term"
-    "golang.org/x/sys/unix"
     str "strings"
     "syscall"
     "time"
-//    "fmt"
+
+    term "github.com/pkg/term"
+    "golang.org/x/sys/unix"
+    // "fmt"
 )
 
-
 func procKill(pid int) {
-    syscall.Kill(pid,syscall.SIGINT)
+    syscall.Kill(pid, syscall.SIGINT)
 }
-
 
 func setEcho(s bool) {
     if s {
@@ -27,8 +27,8 @@ func setEcho(s bool) {
 }
 
 func isatty() bool {
-    _ , err := unix.IoctlGetTermios(0, ioctlReadTermios)
-    return err==nil
+    _, err := unix.IoctlGetTermios(0, ioctlReadTermios)
+    return err == nil
 }
 
 func disableEcho() {
@@ -54,7 +54,7 @@ func enableEcho() {
 }
 
 func term_complete() {
-    if tt!=nil {
+    if tt != nil {
         // disable_mouse()
         tt.Restore()
         tt.Close()
@@ -66,8 +66,8 @@ func GetWinInfo(fd int) (i int) {
     return -1
 }
 
-/// get keypresses, filtering out undesired until a valid match found
-func wrappedGetCh(p int,disp bool) (i int) {
+// / get keypresses, filtering out undesired until a valid match found
+func wrappedGetCh(p int, disp bool) (i int) {
 
     var keychan chan int
     keychan = make(chan int, 1)
@@ -82,7 +82,9 @@ func wrappedGetCh(p int,disp bool) (i int) {
             if pasted {
                 break
             }
-            if disp { pf("key : %#v\n",c) }
+            if disp {
+                pf("key : %#v\n", c)
+            }
             if c != nil {
                 switch {
                 case bytes.Equal(c, []byte{2}):
@@ -97,17 +99,17 @@ func wrappedGetCh(p int,disp bool) (i int) {
                     k = 163
                 case bytes.Equal(c, []byte{127}):
                     k = 127 // backspace
-                case bytes.Equal(c, []byte{27,91,53,126}): // pgup
+                case bytes.Equal(c, []byte{27, 91, 53, 126}): // pgup
                     k = 15 // replaces Shift In (SI)
-                case bytes.Equal(c, []byte{27,91,54,126}): // pgdown
+                case bytes.Equal(c, []byte{27, 91, 54, 126}): // pgdown
                     k = 14 // replaces Shift Out (SO)
-                case bytes.Equal(c, []byte{0x1B, 0x5B, 0x31,0x3b,0x32,0x41}): // SHIFT-UP
+                case bytes.Equal(c, []byte{0x1B, 0x5B, 0x31, 0x3b, 0x32, 0x41}): // SHIFT-UP
                     k = 211
-                case bytes.Equal(c, []byte{0x1B, 0x5B, 0x31,0x3b,0x32,0x42}): // SHIFT-DOWN
+                case bytes.Equal(c, []byte{0x1B, 0x5B, 0x31, 0x3b, 0x32, 0x42}): // SHIFT-DOWN
                     k = 210
-                case bytes.Equal(c, []byte{0x1B, 0x5B, 0x31,0x3b,0x32,0x43}): // SHIFT-RIGHT
+                case bytes.Equal(c, []byte{0x1B, 0x5B, 0x31, 0x3b, 0x32, 0x43}): // SHIFT-RIGHT
                     k = 209
-                case bytes.Equal(c, []byte{0x1B, 0x5B, 0x31,0x3b,0x32,0x44}): // SHIFT-LEFT
+                case bytes.Equal(c, []byte{0x1B, 0x5B, 0x31, 0x3b, 0x32, 0x44}): // SHIFT-LEFT
                     k = 208
                 case bytes.Equal(c, []byte{0x1B, 0x5B, 0x42}): // DOWN
                     k = 10
@@ -151,13 +153,11 @@ func setupDynamicCalls() {
     // this is populated in windows version
 }
 
-
 // race condition, yes... but who arranges concurrent keyboard access?
-var bigbytelist = make([]byte,6*4096)
+var bigbytelist = make([]byte, 6*4096)
 
-
-// get a key press
-func getch(timeo int) ( []byte, bool, bool, string ) {
+/* old version kept for posterity:
+func getch(timeo int) ([]byte, bool, bool, string) {
 
     term.RawMode(tt)
 
@@ -167,8 +167,8 @@ func getch(timeo int) ( []byte, bool, bool, string ) {
     tt.Restore()
 
     // deal with mass input (pasting?)
-    if numRead>6 {
-        return []byte{0},false,true,string(bigbytelist[0:numRead])
+    if numRead > 6 {
+        return []byte{0}, false, true, string(bigbytelist[0:numRead])
     }
 
     // numRead can be up to 6 chars for special input stroke.
@@ -179,7 +179,67 @@ func getch(timeo int) ( []byte, bool, bool, string ) {
     }
     return bigbytelist[0:numRead], false, false, ""
 }
+*/
 
+// get a key press
+func getch(timeo int) ([]byte, bool, bool, string) {
+
+    term.RawMode(tt)
+
+    tt.SetOption(term.ReadTimeout(time.Duration(timeo) * time.Millisecond))
+    numRead, err := tt.Read(bigbytelist)
+
+    tt.Restore()
+
+    if err != nil {
+        // treat as timeout.. separate later, but timeout is buried in here
+        return nil, true, false, ""
+    }
+
+    data := bigbytelist[0:numRead]
+
+    // Check for VTE bracketed paste mode first
+    if bytes.HasPrefix(data, []byte{0x1B, 0x5B, 0x32, 0x30, 0x30, 0x7E}) {
+        // Start of bracketed paste - collect until end marker
+        return collectBracketedPaste()
+    }
+
+    // Fall back to volume-based detection
+    if numRead > 6 {
+        return []byte{0}, false, true, string(data)
+    }
+
+    // numRead can be up to 6 chars for special input stroke.
+    return data, false, false, ""
+}
+
+func collectBracketedPaste() ([]byte, bool, bool, string) {
+    var pasteBuffer []byte
+
+    for {
+        term.RawMode(tt)
+        tt.SetOption(term.ReadTimeout(100 * time.Millisecond))
+        numRead, err := tt.Read(bigbytelist)
+        tt.Restore()
+
+        if err != nil {
+            break
+        }
+
+        data := bigbytelist[0:numRead]
+        pasteBuffer = append(pasteBuffer, data...)
+
+        // Check for end of bracketed paste
+        if bytes.HasSuffix(pasteBuffer, []byte{0x1B, 0x5B, 0x32, 0x30, 0x31, 0x7E}) {
+            // Remove the bracketing markers
+            pasteBuffer = pasteBuffer[6 : len(pasteBuffer)-6]
+            return []byte{0}, false, true, string(pasteBuffer)
+        }
+    }
+
+    // If we get here, something went wrong with bracketed paste
+    return []byte{0}, false, true, string(pasteBuffer)
+}
 
 // GetCursorPos()
 // @note: don't use this if you can avoid it. better to track the cursor yourself
@@ -188,40 +248,40 @@ func getch(timeo int) ( []byte, bool, bool, string ) {
 // also externally dependant upon the vt emulation of the terminal software the
 // program is executed within!
 
-func GetCursorPos() (int,int) {
+func GetCursorPos() (int, int) {
 
-    if tt==nil {
+    if tt == nil {
         // return 0,0
-        return -1,-1
+        return -1, -1
     }
 
-    buf:=make([]byte,15,15)
-    var r,c int
+    buf := make([]byte, 15, 15)
+    var r, c int
 
     term.RawMode(tt)
 
     tt.Write([]byte("\033[6n"))
 
-    n,_:=tt.Read(buf)
+    n, _ := tt.Read(buf)
 
-    if n>0 {
-        endpos:=str.IndexByte(string(buf),'R')
-        if endpos==-1 {
-            r=-1; c=-1
+    if n > 0 {
+        endpos := str.IndexByte(string(buf), 'R')
+        if endpos == -1 {
+            r = -1
+            c = -1
         } else {
-            op:=string(buf[2:endpos])
-            parts:=str.Split(op,";")
-            r,_=GetAsInt(parts[0])
-            c,_=GetAsInt(parts[1])
+            op := string(buf[2:endpos])
+            parts := str.Split(op, ";")
+            r, _ = GetAsInt(parts[0])
+            c, _ = GetAsInt(parts[1])
         }
     }
 
     tt.Restore()
 
-    return r,c
+    return r, c
 
 }
-
 
 // GetSize returns the dimensions of the given terminal.
 func GetSize(fd int) (int, int, error) {
@@ -231,5 +291,3 @@ func GetSize(fd int) (int, int, error) {
     }
     return int(ws.Col), int(ws.Row), nil
 }
-
-
