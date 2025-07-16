18bf40000b43018a644e6deb4a0222b2892119ac
Daniel Horsley <dhorsley@gmail.com>
2025-07-16 11:41:36 +0100

    more bsd fixes

--- lib-network_freebsd.go
+++ lib-network_freebsd.go
@@ -889,18 +889,3 @@ func tcpTraceroute(targetIP string, port int, timeout time.Duration, maxHops int
 
     return hops, nil
 }
-
-// calculateICMPChecksum calculates ICMP checksum
-func calculateICMPChecksum(data []byte) uint16 {
-    var sum uint32
-    for i := 0; i < len(data)-1; i += 2 {
-        sum += uint32(data[i])<<8 + uint32(data[i+1])
-    }
-    if len(data)%2 == 1 {
-        sum += uint32(data[len(data)-1]) << 8
-    }
-    for sum > 0xffff {
-        sum = (sum & 0xffff) + (sum >> 16)
-    }
-    return uint16(^sum)
-}
