ebbf29f6d4d6948ee3dc6a3fa021507529826100
Daniel Horsley <dhorsley@gmail.com>
2025-07-16 11:32:15 +0100

    another build tag update

--- lib-network_unix.go
+++ lib-network_unix.go
@@ -1,5 +1,5 @@
-//go:build !windows
-// +build !windows
+//go:build linux && !freebsd && !openbsd && !netbsd && !dragonfly && !windows && !test
+// +build linux,!freebsd,!openbsd,!netbsd,!dragonfly,!windows,!test
 
 package main
 
